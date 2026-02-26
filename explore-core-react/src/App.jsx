d11651e11ee26f1119fc0388382f7deae9ea564a
kanikanoorsathi <kahinurakter504@gmail.com>
2026-02-26 10:28:59 +0600

    update message

--- explore-core-react/src/App.jsx
+++ explore-core-react/src/App.jsx
@@ -1,16 +1,41 @@
-
 import './App.css'
 
 function App() {
-  
 
   return (
     <>
-      
       <h1>Vite + React</h1>
+      <Developer name="Mozumder" tech="JS"></Developer>
+      <Developer name="Rajon" tech="python"></Developer>
+      <Developer name="Shabana" tech="Java"></Developer>
+      <Player></Player>
       
-     
     </>
+  )
+}
+
+function Developer (props){
+  console.log(props);
+  return(
+    <div style={{
+        border:'2px solid green',
+        borderRadius: '20px'
+      }}>
+        <h3>Developer: {props.name}</h3>
+        <p>Technology: {props.tech}</p>
+      
+    </div>
+  
+  )
+  
+}
+
+function Player(){
+  return(
+    <div className= 'student'>
+      <h3>Name: </h3>
+      <p>run: </p>
+    </div>
   )
 }
 
