928fa15e4231d8e939bbc0373c5f1c995b609819
kanikanoorsathi <kahinurakter504@gmail.com>
2026-02-26 11:04:51 +0600

    Salami added

--- explore-core-react/src/App.jsx
+++ explore-core-react/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
       <Developer name="Mozumder" tech="JS"></Developer>
       <Developer name="Rajon" tech="python"></Developer>
       <Developer name="Shabana" tech="Java"></Developer>
-      <Player></Player>
+      <Player name= "tamim" run= "5000"></Player>
+      <Player name= "mushi" run= "5000"></Player>
+      <Salami event= "Roja Eid" amount="20"></Salami>
+      <Salami event= "Graduation"></Salami>
       
     </>
   )
@@ -18,6 +21,7 @@ function Developer (props){
   console.log(props);
   return(
     <div style={{
+        margin: '10px',
         border:'2px solid green',
         borderRadius: '20px'
       }}>
@@ -30,13 +34,24 @@ function Developer (props){
   
 }
 
-function Player(){
+function Salami({event, amount=500}){
   return(
-    <div className= 'student'>
-      <h3>Name: </h3>
-      <p>run: </p>
+    <div className='student'>
+      <h3>Salami For:{event}</h3>
+      <p>Amount {amount}</p>
     </div>
   )
 }
+//const {name, run} = {name: 'tamim', run: '5000'}
 
+function Player({name, run}){
+  //console.log(props)
+  return(
+    <div className= 'student'>
+      <h3>Name: {name} </h3>
+      <p>run: {run}</p>
+    </div>
+  )
+}
+// const {name, tech} = {name: 'Mozumder', tech: 'js'}
 export default App
