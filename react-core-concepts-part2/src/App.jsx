f78c9e74b43239f923d712036fe7e425f69ec0d9
kanikanoorsathi <kahinurakter504@gmail.com>
2026-02-28 15:52:05 +0600

    friends added

--- react-core-concepts-part2/src/App.jsx
+++ react-core-concepts-part2/src/App.jsx
@@ -1,9 +1,22 @@
 import Counter from './Counter'
 import Batsman from './Batsman'
-
+import Users from './Users'
+import Friends from './Friends'
 import './App.css'
+import { Suspense } from 'react'
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+
+
+const fetchFriends =  async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  return res.json();
+}
 
 function App() {
+
+  const friendsPromise = fetchFriends();
 
   function handleClick(){
     alert('I am clicked')
@@ -20,6 +33,12 @@ function App() {
   return (
     <>
       <h3>Vite + React</h3>
+      {/* <Suspense fallback={<h3>Loading.....</h3>}>
+        <Users fetchUsers ={fetchUsers}></Users>
+      </Suspense> */}
+      <Suspense fallback = {<h3>Friends are coming for treat....</h3>}>
+           <Friends friendsPromise ={friendsPromise}></Friends>
+      </Suspense>
 
       <Counter></Counter>
 
